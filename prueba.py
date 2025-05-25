37d86081d740af9b12a20c32b6e7c9c4be6b290f
tcunirienucr <tc665.economia@gmail.com>
2025-05-25 15:29:00 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -40,74 +40,74 @@ df_ubicaciones = cargar_ubicaciones(archivo_ubicaciones)
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="excelintermedio")
     #Contenido del formulario
-    nombre = st.text_input("Nombre completo", key="nombre")
-    edad = st.number_input("Edad", 0, 120, key="edad")
-    correo = st.text_input("Correo electrónico", key="correo")
-    grupo = st.selectbox(
-        "¿Cuál es el número de grupo donde te inscribiste? *",
-        options=["", "Grupo 01: martes", "Grupo 02: jueves", "ACTIM"],
-        key="grupo"
-    )
-    asististe = st.radio(
-        "¿Asististe a las cuatro clases? *",
-        options=["Si", "No"],
-        key="asististe"
-    )
-    motivo_ausencia = st.multiselect(
-        "Si faltaste a algunas de las clases, ¿por qué fue? *",
-        options=[
-            "Tuve problemas de internet",
-            "Tuve que atender otras situaciones",
-            "Sentía que las clases no me eran útiles",
-            "No entendía la materia",
-            "No me gustaban las clases",
-            "El horario me resultaba muy incómodo",
-            "No falté a ninguna clase",
-            "Otros"
-        ],
-        key="motivo_ausencia"
-    )
-    clase_favorita = st.text_area("¿Cuál de las clases te gustó más? Porfa contanos por qué *", key="clase_favorita")
-    clase_menos_gusto = st.text_area("¿Cuál de las clases te gustó menos? Porfa contanos por qué *", key="clase_menos_gusto")
-    recomendaciones = st.text_area("¿Qué recomendaciones nos harías para el futuro? *", key="recomendaciones")
-    experiencia = st.text_area("¿Podrías escribir unas pocas líneas comentándonos tu experiencia y resumiéndonos cuál ha sido tu apreciación general del curso? *", key="experiencia")
-    calificacion = st.slider(
-        "En general, ¿qué calificación le das al curso? Donde 1 es la peor nota y 10 es la mejor nota o excelente",
-        1, 10,
-        step=1,
-        format="%d",
-        key="calificacion"
-    )
-    interes_cursos = st.multiselect(
-        "Interés por otros cursos que impartimos y que no hayas llevado (puedes llenar todas las que gustes).",
-        options=[
-            "Economía para la Vida para menores de 20 años",
-            "Redacción Consciente",
-            "Economía para entender el Mercado y la Sociedad",
-            "Indicadores Macroeconómicos",
-            "Ninguno"
-        ],
-        key="interes_cursos"
-    )
-    otro_curso = st.text_input("¿Qué otro curso te gustaría recibir de manera virtual?", key="otro_curso")
-    
-    # Select provincia
-    provincias = df_ubicaciones['Provincia'].unique()
-    provincia = st.selectbox("Provincia", options=[""] + list(provincias), key="provincia")
-    
-    # Select cantón según provincia
-    if provincia:
-        cantones = df_ubicaciones[df_ubicaciones['Provincia'] == provincia]['Cantón'].unique()
-    else:
-        cantones = []
-    canton = st.selectbox("Cantón", options=[""] + list(cantones), key="canton")
-    
-    # Select distrito según cantón
-    if canton:
-        distritos = df_ubicaciones[(df_ubicaciones['Provincia'] == provincia) & (df_ubicaciones['Cantón'] == canton)]['Distrito'].unique()
-    else:
-        distritos = []
-    distrito = st.selectbox("Distrito", options=[""] + list(distritos), key="distrito")
+nombre = st.text_input("Nombre completo", key="nombre")
+edad = st.number_input("Edad", 0, 120, key="edad")
+correo = st.text_input("Correo electrónico", key="correo")
+grupo = st.selectbox(
+    "¿Cuál es el número de grupo donde te inscribiste? *",
+    options=["", "Grupo 01: martes", "Grupo 02: jueves", "ACTIM"],
+    key="grupo"
+)
+asististe = st.radio(
+    "¿Asististe a las cuatro clases? *",
+    options=["Si", "No"],
+    key="asististe"
+)
+motivo_ausencia = st.multiselect(
+    "Si faltaste a algunas de las clases, ¿por qué fue? *",
+    options=[
+        "Tuve problemas de internet",
+        "Tuve que atender otras situaciones",
+        "Sentía que las clases no me eran útiles",
+        "No entendía la materia",
+        "No me gustaban las clases",
+        "El horario me resultaba muy incómodo",
+        "No falté a ninguna clase",
+        "Otros"
+    ],
+    key="motivo_ausencia"
+)
+clase_favorita = st.text_area("¿Cuál de las clases te gustó más? Porfa contanos por qué *", key="clase_favorita")
+clase_menos_gusto = st.text_area("¿Cuál de las clases te gustó menos? Porfa contanos por qué *", key="clase_menos_gusto")
+recomendaciones = st.text_area("¿Qué recomendaciones nos harías para el futuro? *", key="recomendaciones")
+experiencia = st.text_area("¿Podrías escribir unas pocas líneas comentándonos tu experiencia y resumiéndonos cuál ha sido tu apreciación general del curso? *", key="experiencia")
+calificacion = st.slider(
+    "En general, ¿qué calificación le das al curso? Donde 1 es la peor nota y 10 es la mejor nota o excelente",
+    1, 10,
+    step=1,
+    format="%d",
+    key="calificacion"
+)
+interes_cursos = st.multiselect(
+    "Interés por otros cursos que impartimos y que no hayas llevado (puedes llenar todas las que gustes).",
+    options=[
+        "Economía para la Vida para menores de 20 años",
+        "Redacción Consciente",
+        "Economía para entender el Mercado y la Sociedad",
+        "Indicadores Macroeconómicos",
+        "Ninguno"
+    ],
+    key="interes_cursos"
+)
+otro_curso = st.text_input("¿Qué otro curso te gustaría recibir de manera virtual?", key="otro_curso")
+
+# Select provincia
+provincias = df_ubicaciones['Provincia'].unique()
+provincia = st.selectbox("Provincia", options=[""] + list(provincias), key="provincia")
+
+# Select cantón según provincia
+if provincia:
+    cantones = df_ubicaciones[df_ubicaciones['Provincia'] == provincia]['Cantón'].unique()
+else:
+    cantones = []
+canton = st.selectbox("Cantón", options=[""] + list(cantones), key="canton")
+
+# Select distrito según cantón
+if canton:
+    distritos = df_ubicaciones[(df_ubicaciones['Provincia'] == provincia) & (df_ubicaciones['Cantón'] == canton)]['Distrito'].unique()
+else:
+    distritos = []
+distrito = st.selectbox("Distrito", options=[""] + list(distritos), key="distrito")
 
 # Campos del formulario (igual)
 with st.form(key="evaluacionfinal"):
